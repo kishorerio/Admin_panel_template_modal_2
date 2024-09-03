93a51c1ace23b388b35e8659b18a03b127df55f4
kishore <91112126+kishorerio@users.noreply.github.com>
2024-09-03 13:15:01 +0530

    done

---
+++ react-admin-dashboard/src/scenes/invoices/index.jsx
@@ -0,0 +1,15 @@
+import { Box } from "@mui/material";
+import { Header } from "../../components";
+
+const Invoices = () => {
+  return (
+    <Box m="20px">
+      <Header title="INVOICES" subtitle="List of Invoice Balances" />
+      <Box>
+       Invocies
+      </Box>
+    </Box>
+  );
+};
+
+export default Invoices;
---
+++ react-admin-dashboard/src/scenes/team/index.jsx
@@ -0,0 +1,14 @@
+import { Box, Typography, useTheme } from "@mui/material";
+import { Header } from "../../components";
+
+const Team = () => {
+  
+  return (
+    <Box m="20px">
+      <Header title="TEAM" subtitle="Managing the Team Members" />
+      <Box>Team</Box>
+    </Box>
+  );
+};
+
+export default Team;
