63943cbaf0d0c3780be46c4d6373486abbc7a45d
kishore <91112126+kishorerio@users.noreply.github.com>
2024-09-03 13:17:00 +0530

    Add files via upload

---
+++ src/scenes/contacts/index.jsx
@@ -0,0 +1,16 @@
+import { Box } from "@mui/material";
+import { Header } from "../../components";
+
+const Contacts = () => {
+  return (
+    <Box m="20px">
+      <Header
+        title="CONTACTS"
+        subtitle="List of Contacts for Future Reference"
+      />
+      <Box>Contacts</Box>
+    </Box>
+  );
+};
+
+export default Contacts;
---
+++ src/scenes/dashboard/index.jsx
@@ -0,0 +1,7 @@
+import { Box } from "@mui/material";
+
+function Dashboard() {
+  return <Box m="20px">Dashboard</Box>;
+}
+
+export default Dashboard;
